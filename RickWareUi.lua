7537bcf5f5ca970cdf6fb1b68060f127c27fcf50
shakar <136544760+shakar60@users.noreply.github.com>
2024-12-25 20:44:08 +0300

    Update RickWareUi.lua

--- RickWareUi.lua
+++ RickWareUi.lua
@@ -2652,7 +2652,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 100, 0, -75);
+        Position = UDim2.new(0, 100, 0, 15);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
